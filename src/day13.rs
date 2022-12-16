0171844b90bc42e89e795b6530049de04b8d59ab
Mitch Powell <mitchpowell93@gmail.com>
2022-12-16 00:59:45 -0500

    Day 15 + Day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,62 +1,184 @@
 use std::fs;
+use std::fmt::{Debug, Formatter, Result};
+use std::time::Instant;
+use std::cmp::Ordering;
 
-const FILE_PATH: &str = "inputs/day13_input_test.txt";
+const FILE_PATH: &str = "inputs/day13_input.txt";
 
-enum Value {
-    List(Vec<Value>),
+#[derive(Clone)]
+enum Packet {
+    List(Vec<Packet>),
     Number(i32),
 }
 
-// fn parse_line(line: &str) -> Value {
-//     let packet_stack = vec!();
-//     let mut i = 0
-//     for i in 0..line.len() {
-//         match &line[i..i+1]
-//         if &line[i..i+1]
-//     }
-//     match &line[0..1] {
-//         "[" => {
-//             let mut nested_depth = 0;
-//             let mut values = vec!();
-//             for i in 1..line.len() {
-//                 if &line[i..i+1] == "]" {
-//                     nested_depth -= 1;
-//                     if nested_depth == 0 {
-//                         return Value::List(parse_line(&line[1..i]));
-//                     }
-//                 }
-//             }
-//             Value::Number(0)
-//         },
-//         _ => Value::Number(line.parse().unwrap())
-//     }
-// }
+impl<'a> Debug for Packet {
+    fn fmt(&self, formatter: &mut Formatter<'_> ) -> Result {
+        match self {
+            Packet::List(ls) => {
+                formatter.write_str("[")?;
+                for i in 0..ls.len() {
+                    formatter.write_fmt(format_args!("{:?}", ls.get(i).unwrap()))?;
+                    if i != ls.len() - 1 {
+                        formatter.write_str(", ").unwrap();
+                    }
+                }
+                formatter.write_str("]")?;
+            },
+            Packet::Number(n) => {
+                formatter.write_fmt(format_args!("{:?}", n))?;
+            }
 
-// fn parse_input(input: &str) {
-//     let out = input
-//         .split_whitespace()
-//         .map(parse_line);
-// }
-
-fn main() {
-    let contents = fs::read_to_string(FILE_PATH).expect("Could not read input for day13");
-    let parsed = contents.trim().split("\n\n");
-    // let p1 =  part_one(parsed);
-    // println!("D13P1: {p1:?}");
+        }
+        Ok(())
+    }
 }
 
-// fn compare_pairs(left: &str, right: &str) -> bool {
-//     println!("Left: {left:?}");
-//     println!("Right: {right:?}");
-//     let (mut left_chars, mut right_chars) = (left.chars(), right.chars());
-//     let (mut left_depth, mut right_depth) = (0,0);
-//     false
-// }
-//
-// fn part_one<'a>(packet_pairs: impl Iterator<Item = &'a str>) -> i32 {
-//     for pair in packet_pairs {
-//         let (p1, p2) = pair.split_once("\n").unwrap();
-//         compare_pairs(p1, p2);
-//     }
-//     0
-// }
+fn parse_line(line: &str) -> Packet {
+    // Assume that the first character is an open brace and start by parsing the second character
+    let mut i = 1;
+    let mut packet_vec = vec!();
+    // Assume that the last character is a closing brace and don't parse it
+    while i < line.len() - 1 {
+        match &line[i..=i] {
+            "[" => {
+                let mut nesting_level = 1;
+                let mut j = i + 1;
+                while nesting_level > 0 {
+                    match &line[j..=j] {
+                        "[" => {
+                            nesting_level += 1;
+                            j += 1;
+                        },
+                        "]" => {
+                            nesting_level -= 1;
+                            j += 1;
+                        },
+                        _ => {
+                            j += 1
+                        }
+                    }
+                }
+                packet_vec.push(parse_line(&line[i..j]));
+                i = j + 1;
+            },
+            _ => {
+                let mut j = i;
+                for c in line[i..line.len()].chars() {
+                    j += 1;
+                    if !c.is_numeric() {
+                        break;
+                    }
+                }
+                packet_vec.push(Packet::Number(line[i..j - 1].parse().unwrap()));
+                i = j;
+            }
+        }
+    }
+    Packet::List(packet_vec)
+}
+
+fn main() {
+    let start = Instant::now();
+    let contents = fs::read_to_string(FILE_PATH).expect("Could not read input for day13");
+    let mut parsed: Vec<Packet> = contents.trim().lines().filter(|&l| !l.is_empty()).map(parse_line).collect();
+    let p1 = part_one(&parsed[..]);
+    parsed.push(Packet::List(vec!(Packet::Number(2))));
+    parsed.push(Packet::List(vec!(Packet::Number(6))));
+    let p2 = part_two(&mut parsed[..]);
+    println!("Elapsed: {:?}", start.elapsed());
+    println!("D13P1: {p1:?}");
+    println!("D13P2: {p2:?}");
+}
+
+fn compare_packets(left: &Packet, right: &Packet) -> Ordering {
+    let left = if let Packet::List(ls1) = left { ls1 } else { unreachable!() };
+    let right = if let Packet::List(ls2) = right { ls2 } else { unreachable!() };
+    let (mut i, mut j) = (0, 0);
+    while i < left.len() && j < right.len() {
+        let left = left.get(i).unwrap();
+        let right = right.get(j).unwrap();
+        if let Packet::List(_) = left{
+            if let Packet::List(_) = right {
+                let res = compare_packets(left, right);
+                match res {
+                    Ordering::Equal => {
+                        i += 1;
+                        j += 1;
+                    },
+                    _ => return res,
+                }
+            } else {
+                let r_val = if let Packet::Number(p) = right { p } else { unreachable!() };
+                let res = compare_packets(left, &Packet::List(vec!(Packet::Number(*r_val))));
+                match res {
+                    Ordering::Equal => {
+                        i += 1;
+                        j += 1;
+                    },
+                    _ => return res,
+                }
+            }
+        } else {
+            if let Packet::List(_) = right {
+                let l_val = if let Packet::Number(p) = left { p } else { unreachable!() };
+                let res = compare_packets(&Packet::List(vec!(Packet::Number(*l_val))), right);
+                match res {
+                    Ordering::Equal => {
+                        i += 1;
+                        j += 1;
+                    },
+                    _ => return res,
+                }
+            } else {
+                let l_val = if let Packet::Number(l) = left { l } else { unreachable!() };
+                let r_val = if let Packet::Number(r) = right { r } else { unreachable!() };
+                // println!("Comparing {l_val:?} and {r_val:?}");
+                let res = l_val.cmp(r_val);
+                match res {
+                    Ordering::Equal => {
+                        i += 1;
+                        j += 1;
+                    },
+                    _ => return res,
+                }
+            }
+        }
+    }
+    if i >= left.len() && j >= right.len() {
+        return Ordering::Equal;
+    }
+    if i >= left.len() {
+        return Ordering::Less;
+    }
+    return Ordering::Greater
+
+}
+
+fn part_two(packet_pairs: &mut [Packet]) -> i32 {
+    packet_pairs.sort_by(compare_packets);
+    let (mut i1, mut i2) = (0, 0);
+    for (i, p) in packet_pairs.iter().enumerate() {
+        if let Packet::List(ls) = p {
+            if ls.len() == 1 {
+                if let Packet::Number(val) = ls.get(0).unwrap() {
+                    if *val == 6 {
+                        i1 = i + 1;
+                    } else if *val == 2 {
+                        i2 = i + 1;
+                    }
+                }
+            }
+        }
+    }
+    (i1 * i2) as i32
+}
+
+fn part_one(packet_pairs: &[Packet]) -> i32 {
+    packet_pairs
+        .chunks(2)
+        .enumerate()
+        .map(|(i, chunk)| (i, compare_packets(&chunk[0], &chunk[1])))
+        .filter(|(_, o)| *o == Ordering::Less)
+        .map(|(i, _)| (i + 1) as i32)
+        .sum()
+}
---
+++ src/day15.rs
@@ -0,0 +1,203 @@
+use std::fs;
+use std::time::Instant;
+use std::cmp::{min, max};
+
+const FILE_PATH: &str = "inputs/day15_input.txt";
+const SCAN_LINE: i32 = 2000000;
+const DISTRESS_UPPER_BOUND: i32 = 4_000_000;
+const DISTRESS_LOWER_BOUND: i32 = 0;
+
+#[derive(Clone, Copy)]
+struct Point(i32, i32);
+
+impl Point {
+    fn get_distance(&self, other: Point) -> i32 {
+        (self.0 - other.0).abs() + (self.1 - other.1).abs()
+    }
+}
+
+struct Sensor {
+    beacon: Point,
+    location: Point,
+    beacon_distance: i32,
+}
+
+impl Sensor {
+    fn new(location: Point, beacon: Point) -> Self {
+        Sensor { location, beacon, beacon_distance: location.get_distance(beacon) }
+    }
+}
+
+#[derive(Debug)]
+struct ScanRange {
+    min: i32,
+    max: i32,
+}
+impl ScanRange {
+    fn new(min: i32, max: i32) -> Self {
+        ScanRange { min, max }
+    }
+
+    fn intersects(&self, other: &ScanRange) -> bool {
+        return
+            self.min >= other.min && self.min <= other.max || 
+            self.max >= other.min && self.max <= other.max ||
+            other.max >= self.min && other.max <= self.max ||
+            other.min >= self.min && other.min <= self.max
+    }
+    fn contains(&self, val: i32) -> bool {
+        self.min <= val && self.max >= val
+    }
+
+}
+
+#[derive(Debug)]
+struct Scanned {
+    ranges: Vec::<ScanRange>
+}
+
+impl Scanned {
+    fn new() -> Self {
+        Scanned { ranges: vec!() }
+    }
+    fn merge_ranges(&mut self) {
+        let mut i = 0;
+        let mut j = 1;
+        loop {
+            if j >= self.ranges.len() {
+                return
+            }
+            if self.ranges[i].intersects(&self.ranges[j]) {
+                self.ranges[i].min = min(self.ranges[j].min, self.ranges[i].min);
+                self.ranges[i].max = max(self.ranges[j].max, self.ranges[i].max);
+                self.ranges.remove(j);
+                j = i + 1;
+            } else {
+                j += 1;
+                if j == self.ranges.len() {
+                    i += 1;
+                    j = i + 1;
+                }
+            }
+        }
+    }
+    fn insert(&mut self, rng1: ScanRange) {
+        for rng2 in self.ranges.iter_mut() {
+            if rng2.intersects(&rng1) {
+                rng2.min = min(rng1.min, rng2.min);
+                rng2.max = max(rng1.max, rng2.max);
+                self.merge_ranges();
+                return;
+            }
+        }
+        self.ranges.push(rng1);
+    }
+
+    fn contains(&self, val: i32) -> bool {
+        self.ranges.iter().any(|rng| rng.contains(val))
+    }
+
+    fn remove(&mut self, v: i32) {
+        for i in 0..self.ranges.len() {
+            let rng = &mut self.ranges[i];
+            if rng.min == v && rng.max == v {
+                self.ranges.remove(i);
+                self.merge_ranges();
+                return; 
+            }
+            if rng.min == v {
+                rng.min = v + 1;
+                self.merge_ranges();
+                return;
+            }
+            if rng.max == v {
+                rng.max = v - 1;
+                self.merge_ranges();
+                return;
+            }
+            if rng.min < v && rng.max > v {
+                let rng2 = ScanRange::new(v + 1, rng.max);
+                rng.max = v - 1;
+                self.ranges.push(rng2);
+            }
+        }
+    }
+
+    fn size(&self) -> usize {
+        let mut out = 0;
+        for rng in &self.ranges {
+            out += ((rng.max - rng.min) + 1) as usize;
+        }
+        out
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Sensor> {
+    let mut sensors = vec!();
+    for line in input.lines() {
+        let (sensor_str, beacon_str) = line.split_once(": ").unwrap();
+        let (_, x_str) = sensor_str.split_once("x=").unwrap();
+        let (sensor_x, sensor_y) = x_str.split_once(", y=").unwrap();
+        let (_, x_str) = beacon_str.split_once("x=").unwrap();
+        let (beacon_x, beacon_y) = x_str.split_once(", y=").unwrap();
+
+        sensors.push(Sensor::new(
+            Point(sensor_x.parse().unwrap(), sensor_y.parse().unwrap()),
+            Point(beacon_x.parse().unwrap(), beacon_y.parse().unwrap()),
+        ));
+    }
+    sensors
+}
+
+fn main() {
+    let start = Instant::now();
+    let contents = fs::read_to_string(FILE_PATH).expect("Could not read input for day15");
+    let sensors = parse_input(&contents);
+    let p1 = part_one(&sensors, SCAN_LINE, false).size();
+    let p2 = part_two(&sensors);
+    println!("Elapsed: {:?}", start.elapsed());
+    println!("D15P1: {p1:?}");
+    println!("D15P2: {p2:?}");
+}
+
+fn part_one(sensors: &Vec<Sensor>, line: i32, set_bounds: bool) -> Scanned {
+    let mut scanned = Scanned::new();
+    for sensor in sensors {
+        let Point(x, _) = sensor.location;
+        let line_distance = sensor.location.get_distance(Point(x, line));
+        let intersection = sensor.beacon_distance - line_distance;
+        if intersection < 0 {
+            continue
+        }
+        if set_bounds {
+            let min_bound = max(x - intersection, DISTRESS_LOWER_BOUND);
+            let max_bound = min(x + intersection, DISTRESS_UPPER_BOUND);
+            scanned.insert(ScanRange::new(min_bound, max_bound));
+        } else {
+            scanned.insert(ScanRange::new(x-intersection, x + intersection));
+        }
+    }
+    
+    if !set_bounds {
+        for Sensor{ beacon, .. } in sensors {
+            if beacon.1 == line {
+                scanned.remove(beacon.0);
+            }
+        }
+    }
+    scanned
+}
+
+fn part_two(sensors: &Vec<Sensor>) -> u128 {
+    for line in 0..DISTRESS_UPPER_BOUND {
+        let res = part_one(sensors, line, true);
+        if res.size() == DISTRESS_UPPER_BOUND as usize {
+            for x in 0..DISTRESS_UPPER_BOUND {
+                if !res.contains(x) {
+                    return ((x as u128 * 4_000_000 as u128) + line as u128) as u128;
+                }
+            }
+        }
+    }
+    0
+}
